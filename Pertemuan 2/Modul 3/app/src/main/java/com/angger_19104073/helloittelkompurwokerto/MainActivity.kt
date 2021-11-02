da59e69ba43896e9b09fc5a0f37dda7908e5729d
anggeralmasih <angger.almasih@gmail.com>
2021-11-02 17:28:01 +0700

    Pertemuan 2

---
+++ Pertemuan 2/Modul 3/app/src/main/java/com/angger_19104073/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.angger_19104073.helloittelkompurwokerto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPage = findViewById<Button>(R.id.btn_page)
+        btnPage.setOnClickListener {
+            val intent = Intent(this, HalamanActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun printState(msg: String){
+        Log.d("ActivityState", msg) // Logcat, muncul di Logcat Android Studio
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show() //Toast, muncul di UI APP
+    }
+
+    override fun onStart() {
+        super.onStart()
+        printState("Halaman Satu onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        printState("Halaman Satu onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        printState("Halaman Satu onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        printState("Halaman Satu onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        printState("Halaman Satu onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        printState("Halaman Satu onDestroy")
+    }
+
+}
