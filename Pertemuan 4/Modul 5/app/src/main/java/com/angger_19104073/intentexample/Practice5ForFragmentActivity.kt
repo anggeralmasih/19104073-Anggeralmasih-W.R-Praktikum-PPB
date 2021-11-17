d281d15cf3f4fae2e62cb601796f8a5bb4ce897c
anggeralmasih <angger.almasih@gmail.com>
2021-11-17 22:10:47 +0700

    Pertemuan 4

---
+++ Pertemuan 4/Modul 5/app/src/main/java/com/angger_19104073/intentexample/Practice5ForFragmentActivity.kt
@@ -0,0 +1,27 @@
+package com.angger_19104073.intentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.angger_19104073.intentexample.databinding.ActivityPractice5ForFragmentBinding
+
+class Practice5ForFragmentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPractice5ForFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPractice5ForFragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mFragmentManager = supportFragmentManager
+        val mFirstFragment = Practice5FirstFragment()
+        val fragment = mFragmentManager.findFragmentByTag(Practice5FirstFragment::class.java.simpleName)
+
+        if (fragment !is Practice5FirstFragment){
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, mFirstFragment, Practice5FirstFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+}
