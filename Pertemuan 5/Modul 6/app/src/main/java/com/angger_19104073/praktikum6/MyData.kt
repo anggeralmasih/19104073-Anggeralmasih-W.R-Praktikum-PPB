7aa3e3088ed002e9d54c9b34b60dfca0a99e69d0
anggeralmasih <angger.almasih@gmail.com>
2021-12-21 10:35:07 +0700

    Pertemuan 5

---
+++ Pertemuan 5/Modul 6/app/src/main/java/com/angger_19104073/praktikum6/MyData.kt
@@ -0,0 +1,11 @@
+package com.angger_19104073.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name : String,
+    var description : String,
+    var photo : String
+):Parcelable
